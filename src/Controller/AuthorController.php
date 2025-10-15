4e251f060c41f8ae4d1eebeb4864450e0ca5c77a
fares8lefi <fareslefi4@gmail.com>
2025-10-15 20:31:01 +0200

    create add function

--- src/Controller/AuthorController.php
+++ src/Controller/AuthorController.php
@@ -5,6 +5,11 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Entity\Book;
+use App\Form\BookType;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
+
 
 final class AuthorController extends AbstractController
 {
@@ -15,4 +20,21 @@ final class AuthorController extends AbstractController
             'controller_name' => 'AuthorController',
         ]);
     }
+
+     #[Route('/addBook', name: 'app_addBook')]
+    public function addBook(ManagerRegistry $mr ,Request $request): Response
+    {
+       $em=$mr->getManager();  //demande d'acces a l'entity manager
+        $book=new Book();
+        $form=$this->createForm(BookType::class,$book);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $em->persist($book); // prepare la requete
+            $em->flush(); // execute la requete
+    }
+    return $this->render('author/addBook.html.twig', [
+    'formBook' => $form->createView()
+]);
+
+}
 }
